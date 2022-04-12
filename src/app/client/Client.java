68a443e6026ee200d51df7661ca9d2d4fe521890
SalmanDA <alfariqsalman@gmail.com>
2022-04-12 22:09:22 +0700

    fix private and broadcast msg

--- src/app/client/Client.java
+++ src/app/client/Client.java
@@ -1,11 +1,15 @@
 package app.client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import app.message.Message;
 import app.server.ServerThread;
@@ -15,50 +19,64 @@ public class Client {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Client Alive");
-		try {
-			Socket socket = new Socket("localhost",6666);
-			
-			ObjectOutputStream ous = new ObjectOutputStream(socket.getOutputStream());
-			
-			System.out.println("Input request type: ");
-			Scanner msgRequest = new Scanner(System.in);
-			Message msg = new Message(msgRequest.toString());
-			
-			System.out.println("Input sender name: ");
-			Scanner msgSender = new Scanner(System.in);
-			msg.setSender(msgSender.toString());
-			
-			System.out.println("Input receiver name: ");
-			Scanner msgReceiver = new Scanner(System.in);
-			msg.setReceiver(msgReceiver.toString());
-			
-			System.out.println("Input text message: ");
-			Scanner msgText = new Scanner(System.in);
-			msg.setText(msgText.toString());
-			
-			ServerThread st = new ServerThread();
-			
-			if(msg.getRequest() == "Online User") {
-				st.getOnlineUsers();
-				
-			} else if (msg.getRequest() == "Private") {
-				st.sendPrivately(msg, msg.getReceiver());
-			} else {
-				st.sendToAll(msg);
-			}
-			
-			ous.flush();
-			
-			ous.close();
-			socket.close();
-			
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        try {
+            Socket socket = new Socket("127.0.0.1", 9000);
+ 
+            ObjectOutputStream ous = new ObjectOutputStream(socket.getOutputStream());
+            WorkerThread wt = new WorkerThread(new ObjectInputStream(socket.getInputStream()));
+            wt.start();
+ 
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+ 
+            System.out.println("Enter your username: \n>> ");
+            String username = reader.readLine();
+            
+            Message firstMessage = new Message();
+            firstMessage.setText(username);
+            ous.writeObject(firstMessage);
+            ous.flush();
+            
+            while(true) {
+                System.out.println("---Request List Keyword---");
+                System.out.println("> Private");
+                System.out.println("> Broadcast");
+                System.out.println("> Online Users");
+                System.out.println("Enter your request: \n>>");
+                String req = reader.readLine();
+                
+                Message message = new Message();
+                message.setSender(username);
+                message.setRequest(req);
+                
+                if (req.equals("Private")) {
+                    System.out.println("Enter message receiver: \n>> ");
+                    String receiver = reader.readLine();
+                    message.setReceiver(receiver);
+                }
+                else if (req.equals("Broadcast")) {
+                    
+                }
+                else if (req.equals("Online Users")) {
+                    
+                }
+                
+                if (req.equals("Private") || req.equals("Broadcast")) {                 
+                    System.out.println("Enter your message: \n>>");
+                    String text = reader.readLine();
+                    message.setText(text);
+                }
+ 
+                ous.writeObject(message);
+                ous.flush();
+            }
+ 
+//            unreacable statements
+//            ous.close();
+//            socket.close();
+ 
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
 	}
 
 }
